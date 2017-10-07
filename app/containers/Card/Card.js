2516ae5298a304df57ade2c1b9da48217c2fa931
immanoj16 <mmaharana14@gmail.com>
2017-10-07 12:51:15 +0530

    Add: function to convert to string

--- app/containers/Card/Card.js
+++ app/containers/Card/Card.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+
+	nestedString(nestedData) {
+		if (nestedData) {
+			let resultString = ''
+			nestedData.map(data => resultString = resultString + data.name + ', ')
+			resultString = resultString.trim().substring(0, resultString.length - 2)
+			return resultString
+		}
+	}
+
 	render() {
 		const { data } = this.props
 
@@ -20,41 +30,41 @@ class Card extends Component {
 			runtime,
 			spoken_languages,
 			tagline,
-			title,
+			original_title,
 			vote_averages } = this.props.data
 
-		// const posterIMG = 'https://image.tmdb.org/t/p/w500' + data.poster_path
-  //   const productionCompanies = data.production_companies
-  //   const productionCountries = data.production_countries
-  //   const genres = data.genre
-  //   const totalRevenue = data.revenue
-  //   const productionList = nestedDataToString(productionCompanies)
-  //   const productionCountriesList = nestedDataToString(productionCountries)
-  //   const noData = '-'
-  //   const genresList = nestedDataToString(genres)
+			const genre = this.nestedString(genres)
+			const productionCompanies = this.nestedString(production_companies)
+			const productionCountries = this.nestedString(production_countries)
+
+    // const noData = '-'
   //   const backdropIMG = 'https://image.tmdb.org/t/p/original' + data.backdrop_path
 
 		return (
-			<div className="container">
-				<div className="image">
-					<img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
-				</div>
-				<div className="dataContainer">
-
-				</div>
+			<div className="col-xs-12 col-lg-10 offset-lg-1">
+				<div className="col-xs-12 cardcont nopadding">
+		      <div className="meta-data-container col-xs-12 col-md-8 push-md-4 col-lg-7 push-lg-5">
+		        <h1>{original_title}</h1>
+		        <span className="tagline">{tagline}</span>
+		        <p>{overview}</p>
+		        <div className="additional-details">
+		          <span className="genre-list">{genre}</span><br />
+		          <span className="production-list">{productionCompanies}, {productionCountries}</span>
+		          <div className="row nopadding release-details">
+		            <div className="col-xs-6"> Original Release: <span className="meta-data">{release_date}</span></div>
+		            <div className="col-xs-6"> Running Time: <span className="meta-data">{runtime} mins</span> </div>
+		            <div className="col-xs-6"> Box Office: <span className="meta-data">{revenue}</span></div>
+		            <div className="col-xs-6"> Vote Average: <span className="meta-data">{vote_averages}</span></div>
+		          </div>
+		        </div>
+		      </div>
+		      <div className="poster-container nopadding col-xs-12 col-md-4 pull-md-8 col-lg-5 pull-lg-7 ">
+		        <img className='poster' src={`https://image.tmdb.org/t/p/original${poster_path}`} />
+		      </div>
+		    </div>
 			</div>
 		);
 	}
-}
-
-function nestedDataToString(nestedData) {
-  let nestedArray = [],
-      resultString;
-  nestedArray.forEach(function(item, i){
-    nestedArray.push(item.name);
-  });
-  resultString = nestedArray.join(', '); // array to string
-  return resultString;
 }
 
 export default Card
--- app/containers/Nav/NavContainer.js
+++ app/containers/Nav/NavContainer.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
 
+const TMDBLogo = 'https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg';
+
 class NavContainer extends Component {
 
 	render() {
 		return (
-			<div className="navContainer">
-				<ul className="navbar">
-					<li>TMDB-APP</li>
-					<li>
-						<form onSubmit={this.props.submitQuery}>
+			<div className="col-xs-12 search-container nopadding">
+        <div className="row">
+
+          <div className="col-xs-12 col-sm-6 col-lg-5">
+            <a href="./" title="ReactJS TMDb Movie Search"><img src={TMDBLogo} className="logo" alt="The Movie Database" /></a>
+          </div>
+          <div className="col-xs-12 col-sm-6 col-lg-7">
+            <form onSubmit={this.props.submitQuery}>
 							<input type="text" placeholder="search by movie name..." onChange={this.props.changeText} />
 						</form>
-					</li>
-				</ul>
-			</div>
+          </div>
+        </div>
+      </div>
 		);
 	}
 }
