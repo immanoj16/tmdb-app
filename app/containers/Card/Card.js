9883176bafdd48a414a3c8434138848ef329a800
immanoj16 <mmaharana14@gmail.com>
2017-10-08 20:55:52 +0530

    Add: comma between the revenue or boxoffice collection

--- app/containers/Card/Card.js
+++ app/containers/Card/Card.js
@@ -11,6 +11,18 @@ class Card extends Component {
 		}
 	}
 
+	commafy(num) {
+		num = num + ''
+		let str = num.split('.');
+    if (str[0].length >= 5) {
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    if (str[1] && str[1].length >= 5) {
+        str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return '$' + str.join('.');
+	}
+
 	componentDidUpdate() {
 		const backdropIMG = 'https://image.tmdb.org/t/p/original' + this.props.data.backdrop_path
 
@@ -42,6 +54,7 @@ class Card extends Component {
 		const genre = this.nestedString(genres)
 		const productionCompanies = this.nestedString(production_companies)
 		const productionCountries = this.nestedString(production_countries)
+		const boxOffice = this.commafy(revenue)
 
 		return (
 				<div className='container'>
@@ -62,7 +75,7 @@ class Card extends Component {
 		            	<div> Running Time: <span className='data'>{runtime} mins</span> </div>
 		            </div>
 		            <div>
-		            	<div> Box Office: <span className='data'>{revenue}</span></div>
+		            	<div> Box Office: <span className='data'>{boxOffice}</span></div>
 		            	<div> Vote Average: <span className='data'>{vote_average} / 10</span></div>
 		            </div>
 		          </div>
